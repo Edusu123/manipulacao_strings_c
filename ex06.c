d18585217fb2ce48ac3b4c6a9fc9d29bf4761c26
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2022-03-27 18:33:48 -0300

    :heavy_check_mark: exercício 6

---
+++ ex06.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char palavra[256];
+    printf("palavra: ");
+    scanf("%s", &palavra);
+
+    int len = strlen(palavra);
+
+    int i;
+    for(i = 0; i < (len / 2); i += 1){
+        char aux = palavra[i];
+        palavra[i] = palavra[len - (i + 1)];
+        palavra[len - (i + 1)] = aux;
+    }
+    palavra[len + 1] = '\0';
+
+    printf("\n\npalavra trocada: %s", palavra);
+    
+    return 0;
+}
