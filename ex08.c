9bd014935bd3b4598c4cb0acd6294fb9d012c8ec
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2022-03-27 18:44:46 -0300

    :heavy_check_mark: exercício 8

---
+++ ex08.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    printf("palavra: ");
+    char palavra[256];
+    scanf("%s", &palavra);
+
+    int len = strlen(palavra);
+    int contadorVogais = 0;
+
+    for(int i = 0; i < len; i += 1){
+        if(palavra[i] == 'a'
+        || palavra[i] == 'e'
+        || palavra[i] == 'i'
+        || palavra[i] == 'o'
+        || palavra[i] == 'u') contadorVogais += 1;
+    }
+
+    printf("\n\nNumero de vogais: %d", contadorVogais);
+    
+    return 0;
+}
