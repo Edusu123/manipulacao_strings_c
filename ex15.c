3c1585423aa06b0d838e678956cf4b8e6f736153
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2022-04-04 20:15:11 -0300

    :heavy_check_mark: exercício 15

---
+++ ex15.c
@@ -0,0 +1,29 @@
+/*
+
+Faça um programa que leia uma palvra (máximo de 50 letras) e some um no valor ASCII
+de cada caractere da palavra. Imprima a string resultante.
+
+*/
+
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+
+
+
+int main(){
+
+    fflush(stdin);
+    char palavra[50];
+    printf("palavra: ");
+    gets(palavra);
+
+    for(int i = 0; i < strlen(palavra); i += 1){
+        palavra[i] = palavra[i] + 1;
+    }
+
+    printf("resultado: %s", palavra);
+
+    return 0;
+}
+
