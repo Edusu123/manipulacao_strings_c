6b8a3e3c4fe52a6bb7888583d7c5bfbf7ff52995
eduardopires <eduardo.c.pires@outlook.com>
2022-04-06 12:08:14 -0300

    :heavy_check_mark: exercício 18

---
+++ ex18.c
@@ -0,0 +1,43 @@
+/*
+
+Implemente um programa que faça uso desse Código de César (3 posições), entre com uma string e 
+retorne a string codificada.
+
+*/
+
+#include<conio.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+
+char *codificaString(char *string);
+
+int main(){
+    fflush(stdin);
+    char string[256];
+    printf("palavra: ");
+    scanf("%s", string);
+
+    char *ptr = string;
+
+    char *r = codificaString(ptr);
+
+    printf("resultado: ");
+    while(*r != '\0') {
+        printf("%c", *r);
+        r++;
+    }
+
+    return 0;
+}
+
+char *codificaString(char* string){
+    char *retorno = string;
+
+    while(*retorno != '\0'){
+        *retorno = (*retorno) + 3;
+        retorno++;
+    }
+
+    return string;
+}
